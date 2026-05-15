2194617d0936da8596ab8a95c712bfdd734d70a4
2022ict38 <dilmiapsara151@gmail.com>
2026-05-15 11:53:44 +0530

    implement itemModel.js

---
+++ itemModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+    title: { 
+        type: String,
+        required: true,
+     },
+    description: { 
+        type: String,
+         required: true,
+    },
+    category: { 
+        type: String,
+        required: true,
+    }, 
+    status: { 
+        type: String,
+        required: true,
+    },  
+    location: { 
+        type: String, 
+        required: true,
+    },
+});
+
+export default mongoose.model("items", itemSchema); 
